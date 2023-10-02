537cff06c867732c852df89bbda60f83d5948776
Michael Zanichelli <michael.zanichelli38@gmail.com>
2023-10-02 11:38:42 -0400

    October 2nd

---
+++ Michael Zanichelli - October 2 Code.py
@@ -0,0 +1,24 @@
+def main():
+    flowers = 5
+    trees = 10
+    plants = flowers + trees
+    trees = 20
+    print ('=============================================================================================')
+    print ()
+    print ('Question 1')
+    print(plants)
+
+    number1 = 125
+    number2 = 100
+    number3 = number1 - number2
+    number3 = 10
+    number1 = 20
+    number2 = number1 + number2 + number3
+    print ()
+    print ('Question 2')
+    print(number2)
+    print ()
+    input ('=============================== [ >> Press ENTER to exit. << ] ==============================')
+
+if __name__ == "__main__":
+    main()
