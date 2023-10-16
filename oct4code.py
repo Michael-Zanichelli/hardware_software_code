a16264f31e582cdb469a047b7c11b5a5351315fa
Michael Zanichelli <michael.zanichelli38@gmail.com>
2023-10-16 11:15:50 -0400

    October 4th, 2023 - Adding Numbers

---
+++ oct4code.py
@@ -0,0 +1,19 @@
+def main():
+    number1 = 125
+    number2 = 100
+    number3 = number1 - number2
+    print ('=============================================================================================')
+    print ()
+    print ('Question 1')
+    print(number3)
+    number3 = 10
+    number1 = 20
+    number2 = number1 + number2 + number3
+    print ()
+    print ('Question 2')
+    print(number3 + number2)
+    print ()
+    input ('=============================== [ >> Press ENTER to exit. << ] ==============================')
+
+if __name__ == "__main__":
+    main()
