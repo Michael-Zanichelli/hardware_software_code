f4a263a43d33d859a9ab211257fbc9d1196a73d5
Michael Zanichelli <michael.zanichelli38@gmail.com>
2023-10-16 11:18:39 -0400

    October 4th, 2023 - Integers and Strings

---
+++ oct4code2.py
@@ -0,0 +1,16 @@
+def main():
+    print('=============================================================================================')
+    print()
+    print("This program adds two numbers.")
+    print()
+    number1 = input("Enter the first number > ")
+    print()
+    number2 = input("Enter the second number > ")
+    print()
+    total = int(number1) + int(number2)
+    print("The total is " + str(total) + ".")
+    print()
+    input ('=============================== [ >> Press ENTER to exit. << ] ==============================')
+
+if __name__ == "__main__":
+    main()
