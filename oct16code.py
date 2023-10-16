5584b0a555b6e5e21ca4369785574898559bc103
Michael Zanichelli <michael.zanichelli38@gmail.com>
2023-10-16 12:27:13 -0400

    October 16th, 2023 - Max Number

---
+++ oct16code.py
@@ -0,0 +1,14 @@
+def max(num1, num2):
+    if num1 >= num2:
+        return num1
+    return num2
+
+def main():
+    larger = max(5,1)
+    print("Larger Number:")
+    print(larger)
+    print()
+    input ('=============================== [ >> Press ENTER to exit. << ] ==============================')
+
+if __name__ == "__main__":
+    main()
