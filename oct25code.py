b3323af8a00360ab6c0b0fc57abb91f5ecb93c57
Michael Zanichelli <michael.zanichelli38@gmail.com>
2023-10-25 11:42:18 -0400

    October 25th, 2023 - Stack Test

---
+++ oct25code.py
@@ -0,0 +1,17 @@
+def stack_me_high(num1):
+    if num1 == 0:
+        return num1
+    print("Current count:",num1)
+    num1 += 1
+    stack_me_high(num1)
+
+def main():
+    num1 = stack_me_high(1)
+    print("Current count:",num1)
+
+if __name__ == "__main__":
+    main()
+
+# The nature of this code prevents me from putting an input() command to
+# prevent certain devices from closing immediately upon completion of the
+# task. I apologize if you are unable to view the code due to this.
