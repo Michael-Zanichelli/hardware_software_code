2d4475857f6e5262bb3c0a00332d4dfda8537bc0
Michael Zanichelli <michael.zanichelli38@gmail.com>
2023-10-30 11:46:25 -0400

    October 30th, 2023 - Recursion Test

---
+++ oct30code.py
@@ -0,0 +1,23 @@
+def stack_me(num1):
+    if num1 == 0 | num1 == 10:
+        print("add stack_me: ",num1)
+        print("finished: ",num1)
+        return num1
+    print("add stack_me: ",num1)
+    num1 += 1
+    return_me = stack_me(num1)
+    print("remove stack_me: ",num1,end=" ")
+    print("return_me ",num1)
+
+def main():
+    start = 1
+    num1 = stack_me(start)
+    print("Back in Main ",end=" ")
+    print("remove stack_me: ",num1)
+
+if __name__ == "__main__":
+    main()
+
+# The nature of this code prevents me from putting an input() command to
+# prevent certain devices from closing immediately upon completion of the
+# task. I apologize if you are unable to view the code due to this.
