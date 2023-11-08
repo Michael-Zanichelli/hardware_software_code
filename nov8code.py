4471d0b8121cb13c860e586c3749a217b78a3f01
Michael Zanichelli <michael.zanichelli38@gmail.com>
2023-11-08 11:53:58 -0500

    November 8th, 2023 - Check Hex

---
+++ nov8code.py
@@ -0,0 +1,21 @@
+def check_selection(values):
+    hexs = ["A", "B", "C", "D", "E", "F", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+    for value in values:
+        if value.upper() not in hexs:
+            print()
+            print("Not a hexadecimal!")
+            return(False)
+    return(True)
+
+def main():
+    good_selection = False
+    while not good_selection:
+        selection = input("Provide a hexadecimal number > ")
+        good_selection = check_selection(selection)
+    print()
+    print("Good job,",selection,"is a hexadecimal number!")
+    print()
+    input("Press ENTER to exit.")
+
+if __name__ == "__main__":
+    main()
