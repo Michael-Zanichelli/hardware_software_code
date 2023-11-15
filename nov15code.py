204995be90a7ae711b8d556680685e66d6aed703
Michael Zanichelli <michael.zanichelli38@gmail.com>
2023-11-15 12:35:21 -0500

    November 15th, 2023 - Global Variables

---
+++ nov15code.py
@@ -0,0 +1,11 @@
+Var = 42
+
+def add(num):
+    return(num + Var)
+
+def main():
+    number = input("Enter a number: ")
+    print("Sum:",number,"is a whole number")
+
+if __name__ == "__main__":
+    main()
